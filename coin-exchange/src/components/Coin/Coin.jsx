6a6283b30d616da9369ea7f15ef9e2f2c8e61f4f
TobyKreiselmaier <63912793+TobyKreiselmaier@users.noreply.github.com>
2020-11-29 12:26:46 -0500

    State hooks

--- coin-exchange/src/components/Coin/Coin.jsx
+++ coin-exchange/src/components/Coin/Coin.jsx
@@ -1,14 +1,62 @@
+/* eslint-disable no-useless-constructor */
 import React, { Component } from 'react' //typed rcc
 import './Coin.css'; //create-react-app allows to do this.
 import propTypes from 'prop-types'; //allows checking of props' types
 
 export default class Coin extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        price: this.props.price
+      }
+      this.handleClick = this.handleClick.bind(this)//important to set for each event
+    }
+/* THIS WOULD BE THE AUTOMATED CHANGING OF PRICES
+    componentDidMount() {
+      let callback = () => {
+        //set state to new random value
+        let randomPercentage = 0.995 + Math.random() * 0.01;
+        //don't do this; only allowed constructor:
+        //this.state.price = this.state.price * randomPercentage;
+        //instead do for functions:
+        this.setState( (oldState) => {
+          return {
+            price: oldState.price * randomPercentage
+          };
+        });
+
+        //or this for values, but could be issue with concurrence:
+        //this.setState({price: this.state.price * randomPercentage});
+
+      }
+      setInterval(callback, 1000);
+    }
+*/
+
+handleClick(event) {
+  // Prevent the default action of submitting this form
+  event.preventDefault();
+
+  let randomPercentage = 0.995 + Math.random() * 0.01;
+  this.setState( (oldState) => {
+    return {
+      price: oldState.price * randomPercentage
+    };
+  });
+
+}
     render() {
-        return ( 
+        return (
             <tr className='coinrow'>
               <td>{this.props.name}</td>
               <td>{this.props.ticker}</td>
-              <td>${this.props.price}</td>
+              <td>${this.state.price}</td>
+              <td>
+                <form action='#' method='POST'>
+                  <button onClick={this.handleClick}>Refresh
+                  </button>
+                </form>
+              </td>
             </tr>
         );
       }
